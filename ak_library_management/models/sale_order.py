c953232369b40bae0070b0d5375f665eeb1d1b53
Raj Kalariya <rajkalariya@aktivsoftware.com>
2025-02-19 19:29:38 +0530

    [UPD] update method overriding and changes into bulk_book py file.

--- ak_library_management/models/sale_order.py
+++ ak_library_management/models/sale_order.py
@@ -12,37 +12,42 @@ class SaleOrder(models.Model):
     """
     _inherit = 'sale.order'
 
-    stat = fields.Boolean()
+    check_ok = fields.Boolean(
+        default=False
+    )
     is_approve = fields.Boolean()
 
     def action_confirm(self):
+        """
+        This method is check if product quantity is less than 5 then return validation 
+        error and if more than 5 then proceed base method.
+        """
         if self.is_approve:
             return super().action_confirm()
 
         low_quantity_product = self.order_line.filtered(lambda line: line.product_uom_qty < 5)
         if low_quantity_product:
-            self.stat=True
-            print("\n\n:::::::::::::",self.stat,"\n\n")
-            product_list= "\n".join([line.product_id.display_name for line in low_quantity_product])
+            self.check_ok = True
+            product_list = "\n".join([line.product_id.display_name for line in low_quantity_product])
             raise UserError(_(f"Approval needed! The following books have low stock:\n{product_list}"))
         res = super().action_confirm()
+        print("\n\n\n",self.check_ok)
         return res
 
-
     def approve_order(self):
+        """
+        If is member than approve less than 5 product otherwise 
+        showing UserError
+        """
         if not self.env.user.is_manager:
             raise UserError(_('Only managers can approve orders.'))
-        self.stat = False
+        self.check_ok = False
         self.is_approve = True
 
-    # def reject_order(self):
-    #     # if not self.env.user.is_manager:
-    #     #     raise UserError(_('Only managers can reject orders.'))
-    #     # self.stat = False
-    #     # return super().action_cancel()
-    #     pass
-
     def reject_order(self):
+        """
+        This method is cancel the order.
+        """
         if not self.env.user.is_manager:
             raise UserError(_('Only managers can reject orders.'))
-        self.action_cancel()
+        return super().action_cancel()
