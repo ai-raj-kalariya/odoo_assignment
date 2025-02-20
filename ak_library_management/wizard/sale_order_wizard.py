aca8dea20f32f78d9cd0ab8c2398efb9f1dc8e34
Raj Kalariya <rajkalariya@aktivsoftware.com>
2025-02-20 11:05:07 +0530

    [UPD] update method overriding in wizard.

--- ak_library_management/wizard/sale_order_wizard.py
+++ ak_library_management/wizard/sale_order_wizard.py
@@ -3,6 +3,9 @@ from odoo import fields, models, api, _
 
 
 class SaleOrderWarningWizard(models.TransientModel):
+    """
+    Use to showing warning wizard for low quantity.
+    """
     _name = 'sale.order.warning.wizard'
     _description = 'Sale Order Warning Wizard'
 
@@ -17,7 +20,7 @@ class SaleOrderWarningWizard(models.TransientModel):
     )
 
     def action_mark_approved(self):
-        """ Set check_ok=True in Sale Order but do not confirm it. """
+        """ Set check_ok = True in Sale Order but do not confirm it."""
         if self.sale_order_id:
             self.sale_order_id.write({'check_ok': True})
         return {'type': 'ir.actions.act_window_close'}
